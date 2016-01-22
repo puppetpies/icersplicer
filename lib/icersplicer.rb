432f830920dad2ab105299baa19a88bd722ccbe5
Bri in The Sky <brianh6854@googlemail.com>
2016-01-22 17:04:31 +0000

    Fixed tag issue

--- lib/icersplicer.rb
+++ lib/icersplicer.rb
@@ -14,7 +14,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 6
-    TINY = 4
+    TINY = 0
     CODENAME = "Icicle World !"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end

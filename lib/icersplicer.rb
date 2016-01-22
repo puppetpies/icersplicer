75aa8d86ad352500d6f865547098cade71e216a4
Bri in The Sky <brianh6854@googlemail.com>
2016-01-22 16:20:37 +0000

    Performance improvement release v0.6.2

--- lib/icersplicer.rb
+++ lib/icersplicer.rb
@@ -14,7 +14,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 6
-    TINY = 0
+    TINY = 2
     CODENAME = "Icicle World !"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end

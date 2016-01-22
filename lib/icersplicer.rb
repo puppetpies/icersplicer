bdfdc9cc50613a6f6e79cb31ff2b839d814992a2
Bri in The Sky <brianh6854@googlemail.com>
2016-01-22 23:23:50 +0000

    Quick fix

--- lib/icersplicer.rb
+++ lib/icersplicer.rb
@@ -14,7 +14,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 6
-    TINY = 2
+    TINY = 3
     CODENAME = "Ice Pick !"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end

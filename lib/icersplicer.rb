5a574b83580257723e52826d67a209f53a920447
Bri in The Sky <brianh6854@googlemail.com>
2016-01-23 15:06:40 +0000

    Fixed version

--- lib/icersplicer.rb
+++ lib/icersplicer.rb
@@ -14,7 +14,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 6
-    TINY = 5
+    TINY = 8 
     CODENAME = "Ice Climber !"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end

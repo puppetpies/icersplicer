67d5347398b39712a132d5b62a74c916fef241de
Bri in The Sky <brianh6854@googlemail.com>
2016-01-29 00:06:03 +0000

    Version increase for license change

--- lib/version.rb
+++ lib/version.rb
@@ -4,7 +4,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 9
-    TINY = 6
+    TINY = 8
     CODENAME = "Ice Wolf!"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end

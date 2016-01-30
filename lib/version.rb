e64518a2df2d349d9d5a0865f49914afbb44dbb0
Bri in The Sky <brianh6854@googlemail.com>
2016-01-30 00:29:32 +0000

    Added new version with keywords-ruby.ice

--- lib/version.rb
+++ lib/version.rb
@@ -2,8 +2,8 @@
 module Icersplicer
 
   module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 1 
+    MAJOR = 1
+    MINOR = 0 
     TINY = 0
     CODENAME = "Ice Fox!"
     STRING = [MAJOR, MINOR, TINY].join('.')

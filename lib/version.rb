632fdde0ae551da08dd49130af9b72cf1aefd299
Bri in The Sky <brianh6854@googlemail.com>
2016-01-31 18:04:45 +0000

    Emergency fix for missing lib/globutils.rb

--- lib/version.rb
+++ lib/version.rb
@@ -4,7 +4,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 0 
-    TINY = 4
+    TINY = 6
     CODENAME = "Icey Blizzard!"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end

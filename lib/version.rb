dea99c24061801900b050fad63ebcfd5404b3868
Bri in The Sky <brianh6854@googlemail.com>
2016-02-01 21:17:49 +0000

    Version bump for Milestone 3

--- lib/version.rb
+++ lib/version.rb
@@ -4,7 +4,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 0 
-    TINY = 6
+    TINY = 7 
     CODENAME = "Icey Blizzard!"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end

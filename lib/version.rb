9708e8c912b24c09e12a75f35356e92b8cc53f74
Bri in The Sky <brianh6854@googlemail.com>
2016-02-03 13:24:37 +0000

    Version update

--- lib/version.rb
+++ lib/version.rb
@@ -4,7 +4,7 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 0 
-    TINY = 7 
+    TINY = 9
     CODENAME = "Icey Blizzard!"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
